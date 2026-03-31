3110391ed8d3a4fa23c211fe8d29df073aca5af8
SGM24 <beckers.rival@gmail.com>
2026-03-31 19:41:37 -0300

    added .h header for library

---
+++ scr/SynthCore.h
@@ -0,0 +1,28 @@
+class SynthCore{
+    public:
+
+    struct VoiceConfig {
+        const int16_t* sample;
+        uint32_t sample_length;
+        uint32_t index;
+        uint8_t note;
+        uint16_t pitch_bend;
+        uint8_t volume;
+        uint8_t channel;
+        bool looping;
+        bool active;
+    };
+// TODO : id system for voices to prevent ghosting and allow overlap
+    void addVoice(const int16_t* sample,int32_t sample_length, int8_t note, uint32_t pitch_bend, int8_t volume,int8_t channel , bool looping);
+    void removeVoice(int8_t channel, int8_t note);
+    void stepAudio(); // in case you need to control audio manually for hardware tricks, like pwm playback
+    void getAudioBuffer(int16_t* arr, uint8_t size);
+    VoiceConfig Voices[32];
+    
+    private:
+    uint8_t channel_instrument[16];
+    int16_t channel_output[17]; // 17 is master mix
+    float channel_pitch_bend[16];
+    bool active_channels[16];
+    static const uint8_t bend_range = 2;
+};
