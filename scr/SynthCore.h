c1a83c4bce6ad2c579408cac165e67e59f270ddc
Guillermo Beckers <157775435+Mejolov24@users.noreply.github.com>
2026-04-04 01:17:37 -0300

    small update SynthCore.h
    added ifndef for MAX_VOICES (ram allocation) and corrected pitch bend being 32 bits instead of 16. 
    also i forgot to set some variables to unsigned, fixed that too

--- scr/SynthCore.h
+++ scr/SynthCore.h
@@ -1,3 +1,7 @@
+#ifndef MAX_VOICES
+    #define MAX_VOICES 32  // Default value
+#endif
+
 class SynthCore{
     public:
 
@@ -12,12 +16,14 @@ class SynthCore{
         bool looping;
         bool active;
     };
-// TODO : id system for voices to prevent ghosting and allow overlap
-    void addVoice(const int16_t* sample,int32_t sample_length, int8_t note, uint32_t pitch_bend, int8_t volume,int8_t channel , bool looping);
-    void removeVoice(int8_t channel, int8_t note);
+/* TODO : id system for voices to prevent ghosting and allow overlap.
+ Actually, Who needs this? no one in their right mind plays the same note on the same channel twice. */
+
+    void addVoice(const int16_t* sample,uint32_t sample_length, uint8_t note, uint16_t pitch_bend, uint8_t volume,uint8_t channel , bool looping);
+    void removeVoice(uint8_t channel, uint8_t note);
     void stepAudio(); // in case you need to control audio manually for hardware tricks, like pwm playback
     void getAudioBuffer(int16_t* arr, uint8_t size);
-    VoiceConfig Voices[32];
+    VoiceConfig Voices[MAX_VOICES];
     
     private:
     uint8_t channel_instrument[16];
